31f52302551d64cd39395c0c442594f2bff0787c
JenPingLee <99060653+JenPingLee@users.noreply.github.com>
2022-07-17 22:12:01 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.markdown("# HCC subgroup classifier")
+st.markdown("> This classifier allows you to identify HCC subgroups based on the expression of 26 genes involved in androgen metabolic process pathway. You can choose to upload a csv file for multiple samples or manually input values for single sample.")
+st.markdown("---")
+
+st.sidebar.markdown("# Data Input")
+st.sidebar.subheader("Multiple samples")
+
+
